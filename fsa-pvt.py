5eb1e8d2c71b3054505d8b66befa19424c39fcfa
gitlonglong <41136373+gitlonglong@users.noreply.github.com>
2023-11-12 16:23:51 +0800

    Create fsa-pvt.py

---
+++ fsa-pvt.py
@@ -0,0 +1,49 @@
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio)
+            self.norm = nn.LayerNorm(dim)
+
+        self.W_Ne = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = self.norm(x_)
+            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+                q = q - q.mean(dim=-1, keepdim=True)
+        k = k - k.mean(dim=-1, keepdim=True)
+        attn = (F.normalize(q, dim=-1) @ k.transpose(-2, -1))
+        attn_P = attn.softmax(dim=-1)
+        attn_N = (-attn).softmax(dim=-1)
+        attn_all = (attn_P + torch.clamp(self.W_Ne, 0, 9) * attn_N) * attn
+        attn_all = self.attn_drop(attn_all)
+        x = (attn_all @ v).transpose(1, 2).reshape(B, N, C)
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
